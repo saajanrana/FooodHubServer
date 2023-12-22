4d2872b9537522b74f905498e4986ad57ab58d03
saajanrana <saajanrana001@gmail.com>
2023-12-22 18:47:20 +0530

    profile route

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = 3000;
 
@@ -18,19 +18,25 @@ const User = mongoose.model('User', {
   fullName: { type: String, required: true, validate: /^[a-zA-Z]{3,}$/ },
   email: { type: String, required: true, unique: true, validate: /^\S+@\S+\.\S+$/ },
   password: { type: String, required: true, validate: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/ },
+  phone:{type:String},
+  address:{type:String},
+  imgurl:{type:String}
 });
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
+
+// jwt 
+const generateToken = (user) => {
+  return jwt.sign({ userId: user._id }, "your_secret_key", { expiresIn: "10h" });
+};
+
+
+
 // Registration endpoint with enhanced validation and error messages
 
-app.get('/',async(req,res)=>{
-    res.json(
-        "hola"
-    );
-})
 app.post('/api/register', async (req, res) => {
   const { fullName, email, password } = req.body;
 
@@ -49,7 +55,7 @@ app.post('/api/register', async (req, res) => {
     errors.password = 'Password must be at least 6 characters long with at least one lowercase letter, one uppercase letter, and one digit.';
   }
 
-  console.log("error>>>",errors);
+
 
   if (Object.keys(errors).length > 0) {
     return res.status(400).json({ errors });
@@ -65,8 +71,9 @@ app.post('/api/register', async (req, res) => {
     // Create a new user with enhanced validation
     const newUser = new User({ fullName, email, password });
     await newUser.save();
+    const token = generateToken(newUser);
 
-    res.status(201).json({ message: 'User registered successfully.' });
+    res.status(201).json({ message: 'User registered successfully.' ,token });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error.' });
@@ -87,6 +94,7 @@ app.post('/api/login', async (req, res) => {
     try {
       // Find the user by email
       const user = await User.findOne({ email });
+     
   
       // Check if the user exists
       if (!user) {
@@ -98,14 +106,99 @@ app.post('/api/login', async (req, res) => {
       if (!isPasswordValid) {
         return res.status(401).json({ message: 'Invalid email or password.' });
       }
+
+      const token = generateToken(user);
   
       // If email and password are valid, send success message
-      res.status(200).json({ message: 'Login successful.' });
+      res.status(200).json({ message: 'Login successful.',token });
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: 'Internal server error.' });
     }
   });
+
+
+
+  // /// validation
+
+  // const validateTokenAndGetUserId = (token) => {
+  //   try {
+  //     const decodedToken = jwt.verify(token, "your_secret_key");
+  //     return decodedToken.userId;
+  //   } catch (error) {
+  //     return null;
+  //   }
+  // };
+
+  // //authentication
+  
+  
+  // const authenticateToken = (req, res, next) => {
+  //   const bearerheader = req.headers["authorization"];
+  //   console.log("barere>>>>>>>>", bearerheader);
+  //   const bearer = bearerheader.split(" ");
+  
+  //   const token = bearer[1];
+  //   console.log("tokennnnn>>>>", token);
+  //   if (!token)
+  //     return res
+  //       .status(401)
+  //       .json({ success: false, message: "Token not provided" });
+  
+  //   jwt.verify(token, "your_secret_key", (err, user) => {
+  //     if (err)
+  //       return res.status(403).json({ success: false, message: "Invalid token" });
+  //     req.user = user;
+  //     next();
+  //   });
+  // };
+app.get('/api/profile', async (req, res) => {
+  const token = req.headers.authorization;
+
+  console.log('token>>>>>>>',token);
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized - Token missing.' });
+  }
+
+  try {
+    // Verify the token
+    const decodedToken = jwt.verify(token,"your_secret_key");
+    console.log('decode????????',decodedToken);
+
+    // Find the user by ID from the token
+    const user = await User.findById(decodedToken.userId);
+
+    // Check if the user exists
+    if (!user) {
+      return res.status(404).json({ message: 'User not found.' });
+    }
+
+    // Return user details
+    res.status(200).json({
+      fullName: user?.fullName,
+      email: user?.email,
+      phoneNumber: user?.phoneNumber,
+      image: user?.image,
+      address: user?.address
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ message: 'Unauthorized - Invalid token.' });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Start the server
 app.listen(port, () => {
