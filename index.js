bb3774e15a318562c41b4d730d6fa18478933b52
saajanrana <saajanrana001@gmail.com>
2023-12-28 17:03:02 +0530

    changes

--- index.js
+++ index.js
@@ -188,18 +188,6 @@ app.get('/api/profile', async (req, res) => {
   }
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
