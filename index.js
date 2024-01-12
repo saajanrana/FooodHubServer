99c439db7a48528be9cf0a9943cfd9b6feeef1d1
saajanrana <saajanrana001@gmail.com>
2024-01-12 12:41:16 +0530

    add some more

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ const User = mongoose.model('User', {
   email: { type: String, required: true, unique: true, /*validate: /^\S+@\S+\.\S+$/ */ },
   password: { type: String, required: true,/* validate: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/ */ },
   phone:{type:String},
-  address:{type:String},
+  city:{type:String},
+  state:{type:String},
   imgurl:{type:String}
 });
 
@@ -38,7 +39,7 @@ const generateToken = (user) => {
 // Registration endpoint with enhanced validation and error messages
 
 app.post('/api/register', async (req, res) => {
-  const { fullName, email, password } = req.body;
+  const { fullName, email, password,phone,city,state } = req.body;
 
   // Enhanced validation with error messages
   const errors = {};
@@ -70,7 +71,7 @@ app.post('/api/register', async (req, res) => {
     }
 
     // Create a new user with enhanced validation
-    const newUser = new User({ fullName, email, password });
+    const newUser = new User({ fullName, email, password,phone,city,state });
     await newUser.save();
     const token = generateToken(newUser);
 
@@ -83,7 +84,7 @@ app.post('/api/register', async (req, res) => {
 
 
 app.post('/api/goregister', async (req, res) => {
-  const { fullName,email, password } = req.body;
+  const { fullName,email, password,phone,city,state } = req.body;
   console.log('data>>>>',req.body);
   try {
     // Check if the email is already registered
@@ -93,7 +94,7 @@ app.post('/api/goregister', async (req, res) => {
     }
 
     // Create a new user with enhanced validation
-    const newUser = new User({fullName, email, password });
+    const newUser = new User({fullName, email, password,phone,city,state });
     await newUser.save();
     const token = generateToken(newUser);
     res.status(201).json({ message: 'User registered successfully.' ,token});
@@ -206,20 +207,96 @@ app.get('/api/profile', async (req, res) => {
     if (!user) {
       return res.status(404).json({ message: 'User not found.' });
     }
-
+   console.log('user>>>',user.password);
     // Return user details
     res.status(200).json({
       fullName: user?.fullName,
       email: user?.email,
-      phoneNumber: user?.phoneNumber,
+      phone: user?.phone,
+      city: user?.city,
+      state: user?.state,
       image: user?.image,
-      address: user?.address
     });
   } catch (error) {
     console.error(error);
     res.status(401).json({ message: 'Unauthorized - Invalid token.' });
   }
 });
+
+
+//edit userdata
+
+
+app.put('/api/edit',async(req,res)=>{
+  const token = req.headers.authorization;
+
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized - Token missing.' });
+  }
+  try {
+    const decodedToken = jwt.verify(token,"your_secret_key");
+    console.log('decode????????',decodedToken);
+    const user = await User.findById(decodedToken.userId);
+    console.log('user>>>>',user);
+    
+
+    // const { id } = req.params;
+    // const { name, phone, imgurl } = req.body;
+    // const userId = req.user.userId;
+  
+    //   const contactId = req.params.id;
+  
+      // // Find the user by ID
+      // const user = await User.findById(userId);
+  
+  
+      // const contact = user.contacts.find(
+      //   (contact) => contact._id.toString() === userId
+      // );
+
+      //  // Update contact properties
+      //  contact.name = name || contact.name;
+      //  contact.phone = phone || contact.phone;
+      //  contact.imgurl = imgurl || contact.imgurl;
+   
+      //  // Save the changes to the user
+      //  await user.save();
+
+      const {fullName,email,phone,city,state} = req.body;
+       user.fullName = fullName;
+       user.email = email;
+       user.phone = phone;
+       user.city = city;
+       user.state = state;
+
+      console.log('hue hue hue>>>>.',fullName,email,phone,city,state);
+      console.log('user>>>>',user);
+
+
+      await user.save();
+
+
+    
+     
+      // if (contact) {
+      //     res.json(contact);
+      // } else {
+      //     res.status(404).json({ error: 'Contact not found' });
+      // }
+  } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+
+
+
+
+
+})
+
+
+
+
 
 // Start the server
 app.listen(port, () => {
